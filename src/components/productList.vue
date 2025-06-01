27b34ccf90e3082a63ff8693792b17fd47aeaef6
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-06-01 13:00:38 +0200

    chore: crear estructura carpetas

---
+++ src/components/productList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Productos</h2>
+        <ul>
+            <li v-for="product in products" :key="product.id">{{ product.name }} - {{ product.price }}€</li>
+        </ul>
+
+    </div>
+
+</template>
+
+<script>
+
+import { ref, onMounted } from 'vue';
+import { getAllProducts } from '../services/productService';
+import type {Product} from '../types/Product';
+
+const products = ref<Product[]>([]);
+
+onMounted(async () => {
+    const { data } = await getAllProducts();
+    products.value = data;
+});
+
+</script>
---
+++ src/services/product.service.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import type { Product } from '../types/Product';
+
+const API = 'http://localhost:3000/products';
+
+export const getAllProducts = () => axios.get<Product[]>(API);
+export const getProductById = (id: number) => axios.get<Product>(`${API}/${id}`);
+export const createProduct = (data: Product) => axios.post(API, data);
+export const updateProduct = (id: number, data: Product) => axios.put(`${API}/${id}`, data);
+export const deleteProduct = (id: number) => axios.delete(`${API}/${id}`);
